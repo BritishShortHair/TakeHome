a2deeb03e31d979ce9e8d26b6534a3b83353ce76
hao.cui <hao.cui@ticketmaster.com>
2019-11-12 20:35:51 -0800

    create a recyclerview instance as the container of restaurant list.

--- app/src/main/java/com/haocui/doordash/MainActivity.java
+++ app/src/main/java/com/haocui/doordash/MainActivity.java
@@ -1,10 +1,14 @@
 package com.haocui.doordash;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
